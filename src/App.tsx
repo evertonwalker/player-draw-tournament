93e6f0170ad3716c5889c566ba8bd5d428a4823c
Everton Walker <everton.walker@mavtek.com>
2023-03-01 10:54:50 -0300

    feat: add player, and list them

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { InputHTMLAttributes, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [players, setPlayers] = useState(Array<{ id: number; name: string }>);
+  const [name, setName] = useState("");
+
+  const addPlayer = () => {
+    if (name) {
+      setPlayers([...players, { id: players.length++, name }]);
+      setName("");
+    }
+  };
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === "Enter") {
+      addPlayer();
+    }
+  };
+
+  const removePlayer = (id: number) => {
+    setPlayers([...players.filter((player) => player.id !== id)]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header */}
+      <div className="container-header">
+        <h2>Sorteio de jogadores</h2>
+      </div>
+
+      {/* Adicionar Jogadores */}
+
+      <div className="players">
+        <div className="container-add-players">
+          <div className="input-players">
+            <label className="label-add-players">
+              Digite o nome dos jogadores
+            </label>
+            <input
+              className="input-add-player"
+              type="text"
+              value={name}
+              onKeyDown={handleKeyDown}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button className="add-player-button" onClick={addPlayer}>
+              Adicionar
+            </button>
+          </div>
+          <div className="Players"></div>
+        </div>
+
+        <div className="container-players">
+          <ul className="list-player-ul">
+            {players.map((player) => {
+              return (
+                <li key={player.id}>
+                  {player.name}
+                  <button
+                    className="remove-player-button"
+                    onClick={() => removePlayer(player.id)}
+                  >
+                    X
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+
+      {/* Jogadores sorteados */}
+
+
+      {/* Times com ROLEs */}
     </div>
   );
 }
