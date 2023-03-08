2b3e870c904109242707e374486bf60bfe7d0467
Everton Walker <everton.walker@mavtek.com>
2023-03-08 20:00:48 -0300

    feat: clean dead code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import img from "./assets/img/League-Of-Legends-Logo-Background-PNG.png";
 
