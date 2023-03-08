5e3a35bc1dcb62ea53fa0deea0ed6b7b0394368a
Everton Walker <everton.walker@mavtek.com>
2023-03-08 19:58:43 -0300

    feat: dont repeat role and ordenate players by equal role

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import img from'./assets/img/League-Of-Legends-Logo-Background-PNG.png';
+import img from "./assets/img/League-Of-Legends-Logo-Background-PNG.png";
 
+type Position = {
+  id: number;
+  name: string;
+};
 
-type Players = {
-  id: number,
-  name: string,
-  position?: string ;
-}
+type Player = {
+  id: number;
+  name: string;
+  position: Position;
+};
 
 function App() {
-  const [players, setPlayers] = useState<Players[]>([]);
+  const [players, setPlayers] = useState<Player[]>([]);
   const [name, setName] = useState("");
-  const [team1, setTeam1] = useState<Players[]>([]);
-  const [team2, setTeam2] = useState<Players[]>([]);
+  const [team1, setTeam1] = useState<Player[]>([]);
+  const [team2, setTeam2] = useState<Player[]>([]);
   const [drawTeams, setDrawTeams] = useState(false);
   const isDisabled = players.length % 2 !== 0 || players.length === 0;
-  
+
   useEffect(() => {
     console.log(team1);
     console.log(team2);
@@ -24,7 +28,10 @@ function App() {
 
   const addPlayer = () => {
     if (name) {
-      setPlayers([...players, { id: players.length++, name}])
+      setPlayers([
+        ...players,
+        { id: players.length++, name, position: { id: 0, name: "" } },
+      ]);
       setName("");
     }
   };
@@ -39,47 +46,69 @@ function App() {
     setPlayers([...players.filter((player) => player.id !== id)]);
     setTeam1([...team1.filter((player) => player.id !== id)]);
     setTeam2([...team2.filter((player) => player.id !== id)]);
-    
+  };
+
+  const getRoleWithoutRepeat = (player: Player, positions: Array<Position>) => {
+    let indexPosition = Math.floor(Math.random() * positions.length);
+
+    if (player.position.name && player.position.name === positions[indexPosition].name) {
+      if(positions.length >= 2) {
+        while (positions[indexPosition].name === player.position.name) {
+          indexPosition = Math.floor(Math.random() * positions.length);
+        }
+      } 
+    }
+    player.position = positions.splice(indexPosition, 1)[0];
   };
 
   const handleDrawTeams = () => {
-    const playerWithPositions: Players[] = players.map(player => ({
-      ...player,
-      position: undefined,
-    })); 
-    
     // embaralhar array de jogadores
-    const shuffledPlayers = playerWithPositions.sort(() => 0.5 - Math.random());
-  
+    const shuffledPlayers = players.sort(() => 0.5 - Math.random());
+
     const team1Length = Math.ceil(shuffledPlayers.length / 2);
-    const team1Players = shuffledPlayers.slice(0, team1Length);
-    const team2Players = shuffledPlayers.slice(team1Length, shuffledPlayers.length);
-   
-    console.log(team1Players,'team1Players ');
-    console.log(team2Players,'team2Players ');
-    
-   // Atribuir posição aleatória para cada jogador
-    const positions = ["mid", "top", "jungle", "sup", "adc"];
-    team1Players.forEach((player) => {
-      const randomPositionIndex = Math.floor(Math.random() * positions.length);
-      player.position = positions.splice(randomPositionIndex, 1)[0];
-    });
-    
-    const positions2 = ["mid", "top", "jungle", "sup", "adc"];
-    
-    team2Players.forEach((player) => {
-      const randomPositionIndex = Math.floor(Math.random() * positions2.length);
-      player.position = positions2.splice(randomPositionIndex, 1)[0];
-      console.log(player,'play');
-      
-    });
+    let team1Players = shuffledPlayers.slice(0, team1Length);
+    let team2Players = shuffledPlayers.slice(
+      team1Length,
+      shuffledPlayers.length
+    );
+
+    console.log(team1Players, "team1Players ");
+    console.log(team2Players, "team2Players ");
+
+    // Atribuir posição aleatória para cada jogador
+    const positions = [
+      { id: 1, name: "Top" },
+      { id: 2, name: "Jungle" },
+      { id: 3, name: "Mid" },
+      { id: 4, name: "Adc" },
+      { id: 5, name: "Supp" },
+    ];
+    const positions2 = [
+      { id: 1, name: "Top" },
+      { id: 2, name: "Jungle" },
+      { id: 3, name: "Mid" },
+      { id: 4, name: "Adc" },
+      { id: 5, name: "Supp" },
+    ];
+
+    console.log(team1Players);
+    team1Players.forEach((player) => getRoleWithoutRepeat(player, positions));
+    team2Players.forEach((player) => getRoleWithoutRepeat(player, positions2));
+
+    team1Players = team1Players.sort(
+      (player1: Player, player2: Player) =>
+        player1.position.id - player2.position.id
+    );
+    team2Players = team2Players.sort(
+      (player1: Player, player2: Player) =>
+        player1.position.id - player2.position.id
+    );
 
     setTeam1(team1Players);
     setTeam2(team2Players);
-  
+
     setDrawTeams(true);
   };
-
 
   return (
     <div className="App">
@@ -88,9 +117,7 @@ function App() {
         {/* <h2>Sorteio de jogadores</h2> */}
         <img src={img} alt="Logo" />
       </div>
-       <h4>
-        número de jogadores {players.length }
-       </h4>
+      <h4>número de jogadores {players.length}</h4>
 
       {/* Adicionar Jogadores */}
 
@@ -100,121 +127,109 @@ function App() {
             <label className="label-add-players">
               Digite o nome dos jogadores
             </label>
-           <div className="container-btn-input">
-            <input
-              className="input-add-player"
-              type="text"
-              value={name}
-              onKeyDown={handleKeyDown}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <button 
-               className="add-player-button"
-               disabled={players.length === 10}
-               onClick={addPlayer}>
-              Adicionar
-            </button>
-            <button 
-               className="add-player-button"
-               disabled={isDisabled}
-               onClick={handleDrawTeams}>
-               Sortear
-            </button>
+            <div className="container-btn-input">
+              <input
+                className="input-add-player"
+                type="text"
+                value={name}
+                onKeyDown={handleKeyDown}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <button
+                className="add-player-button"
+                disabled={players.length === 10}
+                onClick={addPlayer}
+              >
+                Adicionar
+              </button>
+              <button
+                className="add-player-button"
+                disabled={isDisabled}
+                onClick={handleDrawTeams}
+              >
+                Sortear
+              </button>
+            </div>
           </div>
-        </div>
           <div className="Players"></div>
         </div>
 
-      <div className="container-players">
-         { players.length > 0 &&
-        <div className="selected-players">
-          <ul className="list-player-ul">
-            {players.map((player) => {
-              return (
-                <li key={player.id}>
-                  {player.name}
-                  <button
-                    className="remove-player-button"
-                    onClick={() => removePlayer(player.id)}
-                  >
-                    X
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        }
-     <div className="container-times">
-        <div className="time1">
-                  {drawTeams && team1.length > 0 && (
-          <ul className="list-player-ul">
-            <div className="titulo-time">
-               <h1>TIME 1</h1>
-               <div className="titule-roles">
-                  <h3>Nomes</h3>
-                   <h3>Rotas</h3>
-               </div>
-             
+        <div className="container-players">
+          {players.length > 0 && (
+            <div className="selected-players">
+              <ul className="list-player-ul">
+                {players.map((player) => {
+                  return (
+                    <li key={player.id}>
+                      {player.name}
+                      <button
+                        className="remove-player-button"
+                        onClick={() => removePlayer(player.id)}
+                      >
+                        X
+                      </button>
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
-            {team1.map((time1) => {
-              return (
-                <>
-                <div className="content-time1">
-                <li key={time1.id}>
-                  {time1.name}
-                </li>
-                <span >
-                  {time1.position}
-                </span>
-                {/* <br/> */}
-                </div>
-                </>
-              );
-            })}
-          </ul>
-        )}
-      </div> 
-      {drawTeams && team2.length > 0 &&
-              <big>
-                vs
-              </big>
-    }
-      <div className="time2">
-           {drawTeams && team2.length > 0 && (
-          <ul className="list-player-ul">
-               <div className="titulo-time">
-               <h1>TIME 2</h1>
-               <div className="titule-roles">
-                  <h3>Nomes</h3>
-                   <h3>Rotas</h3>
-               </div>
+          )}
+          <div className="container-times">
+            <div className="time1">
+              {drawTeams && team1.length > 0 && (
+                <ul className="list-player-ul">
+                  <div className="titulo-time">
+                    <h1>TIME 1</h1>
+                    <div className="titule-roles">
+                      <h3>Nomes</h3>
+                      <h3>Rotas</h3>
+                    </div>
+                  </div>
+                  {team1.map((time1) => {
+                    return (
+                      <>
+                        <div className="content-time1">
+                          <li key={time1.id}>{time1.name}</li>
+                          <span className="position-player">
+                            {time1.position?.name}
+                          </span>
+                          {/* <br/> */}
+                        </div>
+                      </>
+                    );
+                  })}
+                </ul>
+              )}
             </div>
-            {team2.map((time2) => {
-              return (
-                <>
-                 <div className="content-time1">
-                <li key={time2.id}>
-                  {time2.name}
-                </li>
-                <span>
-                  {time2.position}
-                </span>
-               </div>
-                </>
-              );
-            })}
-          </ul>
-        )}
+            {drawTeams && team2.length > 0 && <big>vs</big>}
+            <div className="time2">
+              {drawTeams && team2.length > 0 && (
+                <ul className="list-player-ul">
+                  <div className="titulo-time">
+                    <h1>TIME 2</h1>
+                    <div className="titule-roles">
+                      <h3>Nomes</h3>
+                      <h3>Rotas</h3>
+                    </div>
+                  </div>
+                  {team2.map((time2) => {
+                    return (
+                      <>
+                        <div className="content-time1">
+                          <li key={time2.id}>{time2.name}</li>
+                          <span className="position-player">
+                            {time2.position?.name}
+                          </span>
+                        </div>
+                      </>
+                    );
+                  })}
+                </ul>
+              )}
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-    </div>
-
-      {/* Jogadores sorteados */}
-
-
-      {/* Times com ROLEs */}
     </div>
   );
 }
